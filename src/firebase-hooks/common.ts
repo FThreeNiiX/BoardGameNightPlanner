6b48fad9e11a0fb5e96e73260f31c439dc549ad2
Brian Daviadoff <brian.daviadoff@fanduel.com>
2022-06-15 15:58:00 -0400

    test

--- src/firebase-hooks/common.ts
+++ src/firebase-hooks/common.ts
@@ -6,15 +6,24 @@ import * as firebaseui from 'firebaseui';
 import { Document } from 'models';
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: 'AIzaSyDWMGvNAvl10PX6nP0Fzar6Jtv1g6RNlyk',
-    authDomain: 'board-game-night-planner.firebaseapp.com',
-    databaseURL: 'https://board-game-night-planner.firebaseio.com',
-    projectId: 'board-game-night-planner',
-    storageBucket: 'board-game-night-planner.appspot.com',
-    messagingSenderId: '622458243132',
-    appId: '1:622458243132:web:7263d1b0197c24d7'
-};
+// var firebaseConfig = {
+//     apiKey: 'AIzaSyDWMGvNAvl10PX6nP0Fzar6Jtv1g6RNlyk',
+//     authDomain: 'board-game-night-planner.firebaseapp.com',
+//     databaseURL: 'https://board-game-night-planner.firebaseio.com',
+//     projectId: 'board-game-night-planner',
+//     storageBucket: 'board-game-night-planner.appspot.com',
+//     messagingSenderId: '622458243132',
+//     appId: '1:622458243132:web:7263d1b0197c24d7'
+// };
+const firebaseConfig = {
+    apiKey: "AIzaSyDBlL7TEN6jOGzbIf5hstcK482yEuI7Mgo",
+    authDomain: "bg-billy.firebaseapp.com",
+    projectId: "bg-billy",
+    storageBucket: "bg-billy.appspot.com",
+    messagingSenderId: "289681515587",
+    appId: "1:289681515587:web:ba08326557b049db31e0f6",
+    measurementId: "G-8PNM0B9NJE"
+  };
 // Initialize Firebase
 const fbApp = firebase.initializeApp(firebaseConfig);
 
